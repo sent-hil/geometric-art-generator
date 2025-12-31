83d675a44db2585437a058b71f0ea8c9ab912690
Senthil Arivudainambi <me@sent-hil.com>
2025-12-31 15:07:50 -0800

    Initial Commit

---
+++ GeometricArtGenerator/GeometricArtGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricArtGeneratorApp.swift
+//  GeometricArtGenerator
+//
+//  Created by Senthil Arivudainambi on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct GeometricArtGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
